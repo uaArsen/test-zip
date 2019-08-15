14a273925f39ed771c6175ad51f387b7cfa7b1f4
uaArsen <arsenzhd@gmail.com>
2019-08-15 10:50:15 +0300

    Update script

--- scripts/create_pr.bash
+++ scripts/create_pr.bash
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-
-repo="https://$DOCS_GITHUB_TOKEN@github.com/uaArsen/test-zip.git"
-repo_name='test-zip'
-current_time=$(date +'%s')
-branch_name=docs-update-$projectName-$current_time
+path=$(pwd)
 while getopts ":p:n:" opt; do
   case $opt in
     p) path="$OPTARG"
@@ -16,19 +12,24 @@ while getopts ":p:n:" opt; do
   esac
 done
 
+repo="https://$DOCS_GITHUB_TOKEN@github.com/uaArsen/test-zip.git"
+repo_name='test-zip'
+current_time=$(date +'%s')
+branch_name=docs-update-$projectName-$current_time
+email="arsenzhd@gmail.com"
+username="uaArsen"
+
 git config --global credential.helper store
-git config --global user.email "arsenzhd@gmail.com"
-git config --global user.name "uaArsen"
+git config --global user.email email
+git config --global user.name username
 git clone $repo $HOME/$repo_name
 
 cd $HOME/$repo_name || exit
 
-git status
 git checkout -b $branch_name
-
 cp $path/README.md $HOME/$repo_name/content/_components/$projectName.md
 git add $HOME/$repo_name/content/_components/$projectName.md
-git commit -m"Update docs for component: $projectName"
+git commit -m "Update docs for component: $projectName"
 git status
 git push -q -u $repo origin $branch_name
 git request-pull master $repo $branch_name
